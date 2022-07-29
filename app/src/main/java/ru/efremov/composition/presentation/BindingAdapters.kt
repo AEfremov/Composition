514a9df6804984cfe55ce477ef475ea671b3d20f
ae <alexandr.efremoff@gmail.com>
2022-07-29 16:56:31 +0300

    GameFragment.kt refactoring with binding adapters.

--- app/src/main/java/ru/efremov/composition/presentation/BindingAdapters.kt
+++ app/src/main/java/ru/efremov/composition/presentation/BindingAdapters.kt
@@ -1,10 +1,18 @@
 package ru.efremov.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import ru.efremov.composition.R
 import ru.efremov.composition.domain.entity.GameResult
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -59,3 +67,34 @@ private fun getSmileResId(winner: Boolean): Int {
     }
 }
 
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, onOptionClickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        onOptionClickListener.onOptionClick(textView.text.toString().toInt())
+    }
+}
--- app/src/main/java/ru/efremov/composition/presentation/GameFragment.kt
+++ app/src/main/java/ru/efremov/composition/presentation/GameFragment.kt
@@ -1,12 +1,9 @@
 package ru.efremov.composition.presentation
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -14,7 +11,6 @@ import androidx.navigation.fragment.navArgs
 import ru.efremov.composition.R
 import ru.efremov.composition.databinding.FragmentGameBinding
 import ru.efremov.composition.domain.entity.GameResult
-import ru.efremov.composition.domain.entity.Level
 
 class GameFragment : Fragment(R.layout.fragment_game) {
 
@@ -32,16 +28,16 @@ class GameFragment : Fragment(R.layout.fragment_game) {
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
 
-    private val tvOptions by lazy {
-        mutableListOf<TextView>().apply {
-            add(binding.tvOption1)
-            add(binding.tvOption2)
-            add(binding.tvOption3)
-            add(binding.tvOption4)
-            add(binding.tvOption5)
-            add(binding.tvOption6)
-        }
-    }
+//    private val tvOptions by lazy {
+//        mutableListOf<TextView>().apply {
+//            add(binding.tvOption1)
+//            add(binding.tvOption2)
+//            add(binding.tvOption3)
+//            add(binding.tvOption4)
+//            add(binding.tvOption5)
+//            add(binding.tvOption6)
+//        }
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,102 +54,40 @@ class GameFragment : Fragment(R.layout.fragment_game) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         observeViewModel()
-        setClickListenersToOptions()
     }
 
     private fun observeViewModel() {
-        viewModel.question.observe(viewLifecycleOwner) {
-            binding.tvSum.text = it.sum.toString()
-            binding.tvLeftNumber.text = it.visibleNumber.toString()
-            for (i in 0 until tvOptions.size) {
-                tvOptions[i].text = it.options[i].toString()
-            }
-        }
-
-        viewModel.percentOfRightAnswers.observe(viewLifecycleOwner) {
-            binding.progressBar.setProgress(it, true)
-        }
-
-        viewModel.enoughCountForRightAnswers.observe(viewLifecycleOwner) {
-            binding.tvAnswersProgress.setTextColor(getColorByState(it))
-        }
-
-        viewModel.enoughPercentForRightAnswers.observe(viewLifecycleOwner) {
-            val color = getColorByState(it)
-            binding.progressBar.progressTintList = ColorStateList.valueOf(color)
-        }
-
-        viewModel.formattedTime.observe(viewLifecycleOwner) {
-            binding.tvTimer.text = it
-        }
-
-        viewModel.minPercent.observe(viewLifecycleOwner) {
-            binding.progressBar.secondaryProgress = it
-        }
-
+//        viewModel.question.observe(viewLifecycleOwner) {
+//            binding.tvSum.text = it.sum.toString()
+//            binding.tvLeftNumber.text = it.visibleNumber.toString()
+//            for (i in 0 until tvOptions.size) {
+//                tvOptions[i].text = it.options[i].toString()
+//            }
+//        }
         viewModel.gameResult.observe(viewLifecycleOwner) {
             launchGameFinishedFragment(it)
         }
-
-        viewModel.progressAnswers.observe(viewLifecycleOwner) {
-            binding.tvAnswersProgress.text = it
-        }
     }
 
-    private fun getColorByState(goodState: Boolean): Int {
-        val colorResId = if (goodState) {
-            android.R.color.holo_green_light
-        } else {
-            android.R.color.holo_red_light
-        }
-        return ContextCompat.getColor(requireContext(), colorResId)
-    }
-
-    private fun setClickListenersToOptions() {
-        for (tvOption in tvOptions) {
-            tvOption.setOnClickListener {
-                viewModel.chooseAnswer(tvOption.text.toString().toInt())
-            }
-        }
-    }
+//    private fun setClickListenersToOptions() {
+//        for (tvOption in tvOptions) {
+//            tvOption.setOnClickListener {
+//                viewModel.chooseAnswer(tvOption.text.toString().toInt())
+//            }
+//        }
+//    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-//    private fun parseArgs() {
-//        requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
-//            level = it
-//        }
-//    }
-
     private fun launchGameFinishedFragment(gameResult: GameResult) {
-//        val args = Bundle().apply {
-//            putParcelable(GameFinishedFragment.KEY_GAME_RESULT, gameResult)
-//        }
         findNavController().navigate(
             GameFragmentDirections.actionGameFragmentToGameFinishedFragment(gameResult)
         )
-//        requireActivity().supportFragmentManager.beginTransaction()
-//            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
-//            .addToBackStack(null)
-//            .commit()
-    }
-
-    companion object {
-
-//        const val NAME = "GameFragment"
-//
-//        const val KEY_LEVEL = "level"
-//
-//        fun newInstance(level: Level): GameFragment {
-//            return GameFragment().apply {
-//                arguments = Bundle().apply {
-//                    putParcelable(KEY_LEVEL, level)
-//                }
-//            }
-//        }
     }
 }
